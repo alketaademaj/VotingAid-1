4f0fdcf066e1720ff4ff7ae8594b83833a36f1e0
Ramppu <46961429+Ramppu@users.noreply.github.com>
2021-01-12 19:07:29 +0200

    okok

--- client/src/components/suggestedCandidates.js
+++ client/src/components/suggestedCandidates.js
@@ -5,52 +5,20 @@ class Suggestions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      suggestions: [],
+      suggestions: []
     };
   }
 
   componentDidMount() {
     var userAnswers = this.props.location.data.answers;
-   axios.post('http://localhost:5000/forms',{data: this.props.location.data.school})
+    var data = {
+       answers: this.props.location.data.answers,
+       school:  this.props.location.data.school
+    };
+
+   axios.post('http://localhost:5000/suggested',{data: data})
      .then(res => {
-       let s = [];
-       for (var i = 0; i < res.data.length; i++) {
-         this.setState({['similarity'+i]: 1});
-         this.setState({['danger'+i]: 1});
-         let candidateArray = [];
-
-         for (var j = 0; j < Object.keys(res.data[i].filledForm).length / 2; j++) {
-           if (userAnswers[j] === res.data[i].filledForm['question' + j]) {
-             this.setState({['similarity'+i]: this.state['similarity'+ i] += 1});
-
-           }
-
-           candidateArray.push(res.data[i].filledForm['question' + j]); //Creating an array from filledForm integers
-           // if(userAnswers[j] >) IN A  CERTAIN RANGE
-
-
-           if( (userAnswers[j]  == 2 && res.data[i].filledForm['question' + j] == -2) || (userAnswers[j]  == -2 && res.data[i].filledForm['question' + j] == 2) ) {
-             this.setState({['danger'+i]: this.state['danger'+ i] += 1});
-           }
-         }
-         let userSum = userAnswers.reduce((result,number) => result+number);
-         let candSum = candidateArray.reduce((result,number) => result+number);
-         console.log("//------------------------------------------------//");
-         console.log('KANDIDAATTI ' + res.data[i].name + ' ' + res.data[i].surname);
-         console.log(userSum + " Nää on vastaajan tulokset");
-         console.log(candSum + " Nää on kandidaatin tulokset");
-         console.log("Samoja Vastauksia " + this.state['similarity'+ i]);
-         console.log("Päinvastasia vastauksia " + this.state['danger' + i] + " DANGER");
-         console.log("//------------------------------------------------//");
-          console.log(userAnswers.length + " VASTATUN FORMIN PITUUS");
-          console.log(Object.keys(res.data[i].filledForm).length / 2 + " VASTATUN KANDIDAATIN PITUUS");
-
-         if ( (userSum >= (candSum * 0.50) || this.state['danger' + i] <= 0.25 * userAnswers.length) && this.state['similarity' + i] >= (0.30 * userAnswers.length) ) {
-           s.push(res.data[i]);
-           this.setState({suggestions: s});
-         }
-       }
-       console.log(this.state);
+       this.setState({suggestions: res.data});
    });
    //JOS HALUTAAN PROSENTUAALISTA VERTAILUA if(userSum >= (candSum * 0.75))
  }
@@ -60,12 +28,12 @@ class Suggestions extends Component {
        return (
          <div className = "candidateSuggestions">
          <h1>Candidates With Matching Values:</h1>
-           {this.state.suggestions.map((candidate, index) => {
-              return (
-                 <h2>{candidate.name} | Similarity Score: {this.state['similarity' + (index)]} | Additive Inverse: {this.state['danger' + (index)]}</h2>
-              );
-           }
-          )}
+          {this.state.suggestions.map((candidate, index) => {
+             return (
+                <h2>{candidate.name} </h2>
+             );
+          }
+         )}
           </div>
        );
     }
