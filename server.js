6c4b07ae5c915937fb2499d0787927a3c7f58b12
Alketa <alketa_1996@hotmail.com>
2021-08-23 23:43:05 +0300

    test hosting

--- server.js
+++ server.js
@@ -366,7 +366,7 @@ app.post('/addQuestion', (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.get('/', function (req, res) {
